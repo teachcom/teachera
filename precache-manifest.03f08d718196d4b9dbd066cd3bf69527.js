8e8f59a47a144eae16460bb1b6850922efb4a48c
akopadung <akopadung>
2020-04-03 13:14:47 +0700

    Updates

---
+++ precache-manifest.03f08d718196d4b9dbd066cd3bf69527.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98af9b0403d4b58ab36b142383750d7",
+    "url": "/teachera/index.html"
+  },
+  {
+    "revision": "b336f9c4a9ba70277649",
+    "url": "/teachera/static/css/main.563b42bf.chunk.css"
+  },
+  {
+    "revision": "6da31bb205dfcc6fae02",
+    "url": "/teachera/static/js/2.ec0667f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/teachera/static/js/2.ec0667f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b336f9c4a9ba70277649",
+    "url": "/teachera/static/js/main.045e2ffc.chunk.js"
+  },
+  {
+    "revision": "be6f43a3249341fcfcb6",
+    "url": "/teachera/static/js/runtime-main.953f3803.js"
+  },
+  {
+    "revision": "8eaa53ab12737df476ec37173e40ac6e",
+    "url": "/teachera/static/media/about.8eaa53ab.jpg"
+  },
+  {
+    "revision": "64abd497835e66543842c7459ccde53d",
+    "url": "/teachera/static/media/img_102.64abd497.jpg"
+  },
+  {
+    "revision": "fdff838ccab422511f68729f7cee1c8c",
+    "url": "/teachera/static/media/img_103.fdff838c.jpg"
+  },
+  {
+    "revision": "111f4415caae356c1cc6576cab354944",
+    "url": "/teachera/static/media/img_104.111f4415.jpg"
+  },
+  {
+    "revision": "4888fc133f878918179c3aec777df661",
+    "url": "/teachera/static/media/img_105.4888fc13.jpg"
+  },
+  {
+    "revision": "2eefd0b77837fc4342f91a2f0b6bd8b1",
+    "url": "/teachera/static/media/img_106.2eefd0b7.jpg"
+  },
+  {
+    "revision": "a1fb9d35c227b650f738cbbc066903fa",
+    "url": "/teachera/static/media/img_107.a1fb9d35.jpg"
+  },
+  {
+    "revision": "62ab0b31c08cbd6d2a6e0d925aa086f7",
+    "url": "/teachera/static/media/img_108.62ab0b31.jpg"
+  },
+  {
+    "revision": "356f76ca0837dceea2594c46358a128b",
+    "url": "/teachera/static/media/img_109.356f76ca.jpg"
+  },
+  {
+    "revision": "68dfa08d9d6d5ef3c00c5a41f36b2e22",
+    "url": "/teachera/static/media/img_110.68dfa08d.jpg"
+  },
+  {
+    "revision": "d3c7b396c5b0e61cfb15ccb53bb177a5",
+    "url": "/teachera/static/media/img_111.d3c7b396.jpg"
+  },
+  {
+    "revision": "1393e556d7e2cc9d68c2baf12ee99221",
+    "url": "/teachera/static/media/img_112.1393e556.jpg"
+  },
+  {
+    "revision": "83a83cffc541790cf15e652f7ea7a5c8",
+    "url": "/teachera/static/media/img_113.83a83cff.jpg"
+  },
+  {
+    "revision": "3f37235aaa799913aff43fde3da41be3",
+    "url": "/teachera/static/media/img_114.3f37235a.jpg"
+  },
+  {
+    "revision": "56a2cceb81a30276ace73abe62bbf7de",
+    "url": "/teachera/static/media/img_115.56a2cceb.jpg"
+  },
+  {
+    "revision": "d77842cff668b20628b6407354a29423",
+    "url": "/teachera/static/media/img_116.d77842cf.jpg"
+  },
+  {
+    "revision": "482528dcbd569b1dd6d3001fd9702ef7",
+    "url": "/teachera/static/media/img_117.482528dc.jpg"
+  },
+  {
+    "revision": "2bc0145d99802c7e15f3c6805dde4249",
+    "url": "/teachera/static/media/img_118.2bc0145d.jpg"
+  },
+  {
+    "revision": "c36517fa262d6396570e3d3a0eb95a4b",
+    "url": "/teachera/static/media/img_119.c36517fa.jpg"
+  },
+  {
+    "revision": "e8cc82be18161868a5f7d5b4fe2d47b4",
+    "url": "/teachera/static/media/img_120.e8cc82be.jpg"
+  },
+  {
+    "revision": "fb95d7130f66e3792136167b39aed951",
+    "url": "/teachera/static/media/img_bg_slide0.fb95d713.jpg"
+  },
+  {
+    "revision": "2e01b436a72d0a351efcaf820b98cfa6",
+    "url": "/teachera/static/media/img_bg_slide_1.2e01b436.JPG"
+  },
+  {
+    "revision": "5bbce33e2725f3d5888fc2ac52ee54f1",
+    "url": "/teachera/static/media/img_bg_slide_2.5bbce33e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test1/precache-manifest.f4a7dbcf1a809ab1ec706da64c5c9176.js"
+  "/teachera/precache-manifest.03f08d718196d4b9dbd066cd3bf69527.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teachera/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
